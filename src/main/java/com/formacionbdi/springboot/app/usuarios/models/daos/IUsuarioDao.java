947de303cdd7bebaee3e7cda977986e06e37eaa7
Christian Galdámez <christian.galdamez@globant.com>
2020-02-18 15:25:38 -0600

    Avances

--- src/main/java/com/formacionbdi/springboot/app/usuarios/models/daos/IUsuarioDao.java
+++ src/main/java/com/formacionbdi/springboot/app/usuarios/models/daos/IUsuarioDao.java
@@ -2,14 +2,19 @@ package com.formacionbdi.springboot.app.usuarios.models.daos;
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
-import com.formacionbdi.springboot.app.usuarios.models.entities.Usuario;
+import com.formacionbdi.springboot.app.usuarios.commons.entities.Usuario;
+
+
 
 @RepositoryRestResource(path = "usuarios")
 public interface IUsuarioDao extends PagingAndSortingRepository<Usuario, Long>{
 	
-	public Usuario findByUserName(String userName);
+	@RestResource(path = "buscar-username")
+	public Usuario findByUserName(@Param("nombre") String userName);
 	
 	@Query("select u from Usuario u where u.userName = ?1")
 	public Usuario obtenerUserName(String userName);
